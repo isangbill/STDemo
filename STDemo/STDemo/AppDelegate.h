14fb13655f064561d11d68f51922386719315a89
Sangbill <isangbill@gmail.com>
2016-08-07 15:03:10 +0800

    初始化

---
+++ STDemo/STDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STDemo
+//
+//  Created by bill on 16/8/7.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
